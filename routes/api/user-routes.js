5c901e3132abc487171123a69a1e1a6ad911bba7
Eric Normann <eric.n@me.com>
2021-03-04 14:27:00 -0500

    add vote model and routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res)=> {
     // Access our User model and run .findAll() method)
     User.findAll({
-        //attributes: { exclude: ['password']}
+        attributes: { exclude: ['password']}
     })
         .then(dbUserData =>res.json(dbUserData))
         .catch(err => {
@@ -18,8 +18,22 @@ router.get('/', (req, res)=> {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password']},
-        where: { id: req.params.id }
+        attributes: { 
+            exclude: ['password'],
+        },
+        where: { id: req.params.id },
+        include: [
+            {
+              model: Post,
+              attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+              model: Post,
+              attributes: ['title'],
+              through: Vote,
+              as: 'voted_posts'
+            }
+        ]
     })
     .then(dbUserData => {
         if(!dbUserData) {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const { title } = require('process');
 
 const app = express();
 const PORT = process.PORT || 3001;
@@ -15,3 +16,4 @@ app.use(routes);
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
+
