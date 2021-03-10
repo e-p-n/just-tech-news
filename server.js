56156582264423f9f2ed67707ff4d45ee48cd8f6
Eric Normann <eric.n@me.com>
2021-03-09 21:43:50 -0500

    add login/logout

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const { title } = require('process');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session');
+require('dotenv').config();
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 
 const app = express();
 const PORT = process.PORT || 3001;
 
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turn on routes
 app.use(routes);
@@ -16,4 +39,3 @@ app.use(routes);
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
-
